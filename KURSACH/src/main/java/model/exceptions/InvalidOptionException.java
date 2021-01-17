193badd4634393456e716fc6c4e0aab87e8ffcba
lamecode <kislaya.sliva@gmail.com>
2021-01-17 13:39:23 +0200

    1 sem and 2 sem

--- KURSACH/src/main/java/model/exceptions/InvalidOptionException.java
+++
@@ -1,7 +0,0 @@
-package model.exceptions;
-
-public class InvalidOptionException extends RuntimeException {
-    public InvalidOptionException(String message) {
-        super(message);
-    }
-}
--- KURSACH/src/main/java/model/exceptions/InvalidTimeException.java
+++
@@ -1,7 +0,0 @@
-package model.exceptions;
-
-public class InvalidTimeException extends RuntimeException {
-    public InvalidTimeException(String message) {
-        super(message);
-    }
-}
